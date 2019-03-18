23bac0d07b2ff81e938ca231f08d737949554624
McTrip777 <mcjakedude@gmail.com>
2019-03-18 12:03:01 -0700

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+const express = require('express');
+const server = express();
+server.use(express.json());
+
+const db = require('./data/db.js');
+
+server.listen(5000, () => 
+    console.log('Server')
+);
+
+server.post('/api/users', (req, res) => {
+    const userInfo = req.body;
+    db.users
+    .add(userInfo)
+    .then(user =>
+        res.status(201).json(user)    
+    )
+    .catch(
+        res.status(500).json({message: 'error'})
+    )
+})
+
+// server.get('/api/users', (req, res) => {
+//     db.users
+//     .find()
+//     .then(
+//         res.status(200).json(users)
+//     )
+//     .catch(
+//         res.status(500).json({message: 'error'})
+//     )
+// })
