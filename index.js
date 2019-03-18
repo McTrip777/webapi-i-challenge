bdf0c51a3b09ae1e75c034117154f4dd0616bbaa
McTrip777 <mcjakedude@gmail.com>
2019-03-18 13:32:17 -0700

    MVP

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ server.use(express.json());
 
 const db = require('./data/db.js');
 
+server.listen(5000, () => 
+    console.log('Server is running')
+);
+
 server.get('/api/users', (req, res) => {
     db.users
     .find()
@@ -17,7 +21,9 @@ server.get('/api/users', (req, res) => {
 server.post('/api/users', (req, res) => {
     const userInfo = req.body;
     console.log('user information', userInfo);
-
+    if(req.body.name === "" || req.body.bio === ""){
+        res.status(400).send('Require Name/Bio')
+    }else{
     db.users
     .insert(userInfo)
     .then(user =>
@@ -26,6 +32,7 @@ server.post('/api/users', (req, res) => {
     .catch(error => {
         res.status(500).json({ message: 'error creating user' });
     });
+}
 });
 
 server.delete('/api/users/:id', (req, res) => {
@@ -39,17 +46,23 @@ server.delete('/api/users/:id', (req, res) => {
     .catch(error => {
         res.status(500).json({ message: 'error deleting user' });
     });
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const change = req.body;
+    console.log(id);
+    db.users
+    .update(id, change)
+    .then(updated => {
+    if (updated) {
+        res.status(200).json(updated);
+        } else {
+        res.status(404).json({ message: 'user not found' });
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: 'error updating user' });
+    });
 })
 
-
-
-
-
-
-
-
-
-
-server.listen(5000, () => 
-    console.log('Server is running')
-);
